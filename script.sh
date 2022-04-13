83ba2f3eacac9c8f06bbbc3e022feac410877ec8
Christopher Bottoms, PhD <Christopher-Bottoms@omrf.org>
2022-04-13 15:17:09 +0000

    Added actual copy of script mentioned in README

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# Get ID from the command line
+sample=$1
+
+# Announce that we're processing said sample
+echo "Processing data for '$sample'"
+
+# Add code here to process the sample
